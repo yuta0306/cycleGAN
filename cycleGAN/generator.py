6d0fc27dd485330c4d681e34ff347b523a922881
Yubo <you-2001-3-6@ezweb.ne.jp>
2021-03-02 11:49:55 +0900

    fix typo

--- cycleGAN/generator.py
+++ cycleGAN/generator.py
@@ -14,38 +14,32 @@ class ResNetGenerator(nn.Module):
         self.output_nc = output_nc
         self.ngf = ngf
 
-        model = [
-            nn.ReflectionPad2d(3),
-            nn.Conv2d(input_nc, ngf, kernel_size=7, padding=0, bias=True),
-            nn.InstanceNorm2d(ngf),
-            nn.ReLU(inplace=True)
-        ]
+        model = [nn.ReflectionPad2d(3),
+                 nn.Conv2d(input_nc, ngf, kernel_size=7, padding=0, bias=True),
+                 nn.InstanceNorm2d(ngf),
+                 nn.ReLU(True)]
 
-        n_downsmapling = 2
-        for i in range(n_downsmapling):
-            mult = 2 ** i
-            model += [
-                nn.Conv2d(ngf * mult, ngf * mult * 2, kernel_size=3,
-                            stride=2, padding=1, bias=True),
-                nn.InstanceNorm2d(ngf * mult * 2),
-                nn.ReLU(inplace=True)
-            ]
+        n_downsampling = 2
+        for i in range(n_downsampling):
+            mult = 2**i
+            model += [nn.Conv2d(ngf * mult, ngf * mult * 2, kernel_size=3,
+                                stride=2, padding=1, bias=True),
+                      nn.InstanceNorm2d(ngf * mult * 2),
+                      nn.ReLU(True)]
 
-        mult = 2 ** n_downsmapling
+        mult = 2**n_downsampling
         for i in range(n_blocks):
-            mult = 2 ** i
             model += [ResNetBlock(ngf * mult)]
 
-        for i in range(n_downsmapling):
-            mult = 2 ** (n_downsmapling - i)
-            model += [
-                nn.ConvTranspose2d(ngf * mult, int(ngf* mult / 2),
-                                    kernel_size=3, stride=2, padding=1,
-                                    output_padding=1, bias=True),
-                nn.InstanceNorm1d(int(ngf * mult / 2)),
-                nn.ReLU(inplace=True)
-            ]
-        
+        for i in range(n_downsampling):
+            mult = 2**(n_downsampling - i)
+            model += [nn.ConvTranspose2d(ngf * mult, int(ngf * mult / 2),
+                                         kernel_size=3, stride=2,
+                                         padding=1, output_padding=1,
+                                         bias=True),
+                      nn.InstanceNorm2d(int(ngf * mult / 2)),
+                      nn.ReLU(True)]
+
         model += [nn.ReflectionPad2d(3)]
         model += [nn.Conv2d(ngf, output_nc, kernel_size=7, padding=0)]
         model += [nn.Tanh()]
